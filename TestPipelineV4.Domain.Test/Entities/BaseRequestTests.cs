41a153216b0ccd2cb1f42313452af84e526892d8
jo03430 <jordan_bal@secura.net>
2023-10-17 12:53:03 -0500

    Initial Commit

---
+++ TestPipelineV4.Domain.Test/Entities/BaseRequestTests.cs
@@ -0,0 +1,15 @@
+﻿using Shouldly;
+using TestPipelineV4.Domain.Entities;
+
+namespace TestPipelineV4.Domain.Test.Entities;
+
+public class BaseRequestTests
+{
+    public class TestCase : BaseRequest
+    {
+        
+    }
+
+    [Test]
+    public void BaseRequestTest() => new TestCase {CorrelationId = "abc"}.CorrelationId.ShouldBe("abc");
+}
---
+++ TestPipelineV4.Domain/TestPipelineV4DomainAutofacRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Secura.Infrastructure.Autofac;
+using Secura.Infrastructure.Repositories;
+using Autofac;
+using Module = Autofac.Module;
+using System.Reflection;
+using Secura.Infrastructure.Services;
+
+namespace TestPipelineV4.Domain;
+
+[ExcludeFromCodeCoverage]
+public class TestPipelineV4DomainAutofacRegistration : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterModule<RepositoryModule>();
+
+        builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+            .AssignableTo<IService>()
+            .AsImplementedInterfaces();
+
+        builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+            .AssignableTo<IRepository>()
+            .AsImplementedInterfaces();
+    }
+}
